6ba080256c99f6c854e650a7dc8bf6569ca18e67
tommytian57 <107600286+tommytian57@users.noreply.github.com>
2023-06-25 11:18:59 -0400

    first commit

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,24 @@
+import { defineUserConfig } from "vuepress";
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/",
+
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "Blog Demo",
+      description: "Tommy Tian's Photography Blog",
+    },
+    "/zh/": {
+      lang: "zh-CN",
+      title: "博客演示",
+      description: "淘米田的摄影博客",
+    },
+  },
+
+  theme,
+
+  // Enable it with pwa
+  // shouldPrefetch: false,
+});
